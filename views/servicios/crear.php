726e174490efe75706c341bef3198ba5222434d1
LuisNoe21 <luisnoe.rodriguez09@gmail.com>
2022-04-25 04:27:27 -0600

    modificando css

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -1,11 +1,12 @@
 
+
+<h1 class="nombre-pagina-crear-servicios">Nuevo Servicio</h1>
+<p class="descripcion-pagina-crear-servicios">Llena todos los campos para añadir un nuevo servicio</p>
+
 <?php
     include_once __DIR__ . '/../templates/barra.php';
     include_once __DIR__ . '/../templates/alertas.php';
 ?>
-
-<h1 class="nombre-pagina-crear-servicios">Nuevo Servicio</h1>
-<p class="descripcion-pagina-crear-servicios">Llena todos los campos para añadir un nuevo servicio</p>
 
 
 <form action="/servicios/crear" method="POST" class="formulario">
